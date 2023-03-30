e9217fea98bba23d4aabc8019c195c3ae93f2e64
Waridi318 <rosenjeri7151@gmail.com>
2023-03-30 23:15:35 +0300

    Adding the namespace

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import sys
-res = 0
-r = len(sys.argv)
-if r > 1:
-    for i in range(1, r):
-        res = res + int(sys.argv[i])
-print("{}".format(res))
+if __name__ == "__main__":
+    res = 0
+    r = len(sys.argv)
+    if r > 1:
+        for i in range(1, r):
+            res = res + int(sys.argv[i])
+    print("{}".format(res))
