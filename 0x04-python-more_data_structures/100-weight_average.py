57d349fd3a30c356fb97956cc654172d34a05518
Waridi318 <rosenjeri7151@gmail.com>
2023-04-06 01:41:38 +0300

    Task 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    nume = 0
+    deno = 0
+    for i in range(len(my_list)):
+        for j in range(len(my_list[i])-1):
+            nume = nume + (my_list[i][j] * my_list[i][j + 1])
+            deno = deno + (my_list[i][j + 1])
+    return(nume / deno)
