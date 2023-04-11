4428668493633f12530947889908be578e2feb43
Waridi318 <rosenjeri7151@gmail.com>
2023-04-11 17:37:27 +0300

    Correction of expected output error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -21,6 +21,6 @@ class Square:
                 raise ValueError("size must be >= 0")
             self.__size = size
         except TypeError as e:
-            print(e)
+            raise
         except ValueError as e:
-            print(e)
+            raise
