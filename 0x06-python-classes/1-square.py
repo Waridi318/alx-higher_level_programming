25699b9953758f239cae473f4f8d52e3a6305f1f
Waridi318 <rosenjeri7151@gmail.com>
2023-04-11 17:04:07 +0300

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This ia a module that defines a square
+"""
+
+
+class Square:
+    """
+    This is a class that defines its attributes
+    """
+    def __init__(self, size):
+        """Constructor for the Square class
+        """
+        self.__size = size
