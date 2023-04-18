5d661ee32fa42991430c8380ac92e6f615051a55
Waridi318 <rosenjeri7151@gmail.com>
2023-04-18 23:10:30 +0300

    Task 2.2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module defines a function is_same_class
+"""
+
+
 def is_same_class(obj, a_class):
     """
     checks if obj is an instance of a_class
@@ -11,5 +16,5 @@ def is_same_class(obj, a_class):
     """
     if isinstance(obj, a_class):
         return True
-    else: 
+    else:
         return False
