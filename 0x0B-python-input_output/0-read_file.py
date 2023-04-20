4deafcd9a581b4070365cbf32c2e6cb47b420578
Waridi318 <rosenjeri7151@gmail.com>
2023-04-20 18:50:30 +0300

    Task 0.3

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
     reads a text file and prints it to stdout
     """
     with open(filename, encoding='utf-8') as f:
-        return(f.read())
+        print(f.read(), end="")
