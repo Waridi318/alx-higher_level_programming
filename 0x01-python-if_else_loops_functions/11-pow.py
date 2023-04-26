4fb123a1e5d40469eb35bc77f087b16e760eabc2
Waridi318 <rosenjeri7151@gmail.com>
2023-04-26 23:20:53 +0300

    Task 11.2

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -6,8 +6,9 @@ def pow(a, b):
         res = 1
         for i in range(b, 0):
             res = res * (1 / a)
+        return res
     else:
         res = 1
         for i in range(1, (b + 1)):
             res *= a
-    return res
+        return round(res, 2)
