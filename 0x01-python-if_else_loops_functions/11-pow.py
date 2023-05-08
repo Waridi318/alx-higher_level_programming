7c05d291c9e8b2286fe5cf316da25eaeb493290a
Waridi318 <rosenjeri7151@gmail.com>
2023-05-08 13:59:14 +0300

    Task 11 corrections

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,11 +2,16 @@
 def pow(a, b):
     if b == 0:
         return(1)
-    elif b < 0:
+    elif b < 0 and a > 0:
         res = 1
         for i in range(b, 0):
             res = res * (1 / a)
-        return '{:.4f}'.format(res)
+        return '{:.2f}'.format(res)
+    elif b < 0 and a < 0:
+        res = 1 
+        for i in range(b, 0):
+            res = res * (1 / a)
+        return res        
     else:
         res = 1
         for i in range(1, (b + 1)):
