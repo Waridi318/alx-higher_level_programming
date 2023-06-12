8b75fc9a0c3031a562756e101cb1f385a9b56aff
Waridi318 <rosenjeri7151@gmail.com>
2023-06-12 21:50:25 +0300

    Corrected task 5 output error

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const isnumber = parseInt(process.argv[2]);
 if (isNaN(isnumber)) {
   console.log('Not a number');
 } else {
-  console.log(`My number is: ${process.argv[2]}`);
+  console.log(`My number: ${process.argv[2]}`);
 }
