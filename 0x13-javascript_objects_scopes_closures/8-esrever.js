688397496511fe2a459d25f8c4800773cead7dd4
Waridi318 <rosenjeri7151@gmail.com>
2023-06-13 13:39:31 +0300

    Tasks 7-9

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  let j = 0;
+  while (j < list.length - 1) {
+    j++;
+  }
+  const newList = [];
+  while (j > -1) {
+    newList.push(list[j]);
+    j--;
+  }
+  return (newList);
+};
---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+let i = -1;
+exports.logMe = function (item) {
+  i++;
+  console.log(`${i}: ${item}`);
+};
