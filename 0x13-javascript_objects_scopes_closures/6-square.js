042a26fdf67274801e5a139a6384bc32dbcbc2ec
Waridi318 <rosenjeri7151@gmail.com>
2023-06-13 12:49:31 +0300

    Tasks 1-6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const BaseSquare = require('./5-square');
+module.exports = class Square extends BaseSquare {
+  charPrint (c) {
+    for (let i = 0; i < this.size; i++) {
+      let line = '';
+      for (let j = 0; j < this.size; j++) {
+        if (c !== undefined) {
+          line += c;
+        } else {
+          line += 'X';
+        }
+      }
+      console.log(line);
+    }
+  }
+};
