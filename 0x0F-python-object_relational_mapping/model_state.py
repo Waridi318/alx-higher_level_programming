48ff6d5cfb3597162cecdfc570c8184e432c452a
Waridi318 <rosenjeri7151@gmail.com>
2023-06-18 23:47:14 +0300

    Updated model_state.py to include its relationship with class City

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,7 +7,7 @@ and an instance Base = declarative_base()
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 engine = create_engine(
     'mysql+mysqldb://root:root@localhost:3306/hbtn_0e_6_usa'
@@ -26,3 +26,4 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref='state')
