40b5147812e966d95cd9235a72e1e2e8676bcb92
Waridi318 <rosenjeri7151@gmail.com>
2023-06-22 17:48:42 +0300

    model_state correction

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,16 +6,16 @@ and an instance Base = declarative_base()
 """
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import Column, Integer, String
+# from sqlalchemy.orm import sessionmaker, relationship
 
-engine = create_engine(
-    'mysql+mysqldb://root:root@localhost:3306/hbtn_0e_6_usa'
-)
+# engine = create_engine(
+#    'mysql+mysqldb://root:root@localhost:3306/hbtn_0e_6_usa'
+# )
 
 Base = declarative_base()
-Session = sessionmaker(bind=engine)
-session = Session()
+# Session = sessionmaker(bind=engine)
+# session = Session()
 
 
 class State(Base):
@@ -26,4 +26,4 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref='state')
+#    cities = relationship("City", backref='state')
