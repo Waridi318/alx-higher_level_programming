dd7cfcf1a2458cd0a80a016ad4f91cd677df01b1
Waridi318 <rosenjeri7151@gmail.com>
2023-06-30 14:28:36 -0700

    Task 5 updated

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends data to server using POST
-curl -s -d "test=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
