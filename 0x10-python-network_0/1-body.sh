d64b5b91f8f48669f96349bb5ce079b6d04d43b2
Waridi318 <rosenjeri7151@gmail.com>
2023-06-30 13:28:02 -0700

    shortened file to only 3 lines long

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# takes in a URL,
-#sends a GET request to the URL,
-#and displays the body of the response
-
+# takes in a URL,sends a GET request to the URL,and displays the body of the respons
 curl -s -i -X GET "$1"
