9355e63506de21a7bd09edb5c28b292b1376e432
Waridi318 <rosenjeri7151@gmail.com>
2023-06-30 16:03:19 -0700

    Task 6 skeleton

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module describes a function to find the peak 
+in a list of values
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    Function to find the peak
+    """
+
+    return(max(list_of_integers))
