af3b73d9c65e12a5f2bcaf993caaec7407350b90
Waridi318 <rosenjeri7151@gmail.com>
2023-07-04 15:12:58 +0300

    Task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module fetches https://alx-intranet.hbtn.io/status
+"""
+
+
+from urllib.request import urlopen, Request
+
+req = Request('https://alx-intranet.hbtn.io/status')
+
+with urlopen(req) as response:
+    body = response.read()
+
+print("Body response:")
+print(f"\t- type: {type(body)}\n\t- content: {body}\n\t- utf8 content: {body.decode()}")
