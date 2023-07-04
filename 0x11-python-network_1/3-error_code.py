70c7e9e3179fd9e1579055e2f36468157ad5f5e9
Waridi318 <rosenjeri7151@gmail.com>
2023-07-04 17:33:53 +0300

    Task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,7 +9,7 @@ from sys import argv
 from urllib.request import urlopen
 from urllib.error import HTTPError
 
-if __name__ == "__main__"    :
+if __name__ == "__main__":
     try:
         with urlopen(argv[1]) as response:
             print(response.read().decode('utf-8'))
